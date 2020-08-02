48099ac8dfb2853cceb33428ff7923df2203028b
davfru <davide.fruci@assistdigital.com>
2020-08-02 16:28:26 +0200

    create production docker file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellooo poooppp
+          Hello!
         </p>
         <a
           className="App-link"
